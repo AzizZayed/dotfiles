23887ce01da53c1d64afe47954f2cd764eba14eb
Abd-El-Aziz Zayed <azayed01@gmail.com>
2026-02-26 00:20:12 -0500

    [nvim] edit snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,12 +5,10 @@ return {
     lazy = false,
     ---@type snacks.Config
     opts = {
-      animate = { enabled = true }, -- Efficient animation library
       bigfile = { enabled = true }, -- Disable features when opening large files
       bufdelete = { enabled = true }, -- Delete buffers without disrupting window layout.
       dim = { enabled = true }, -- Dim all except active scope
-      health = { enabled = true }, -- Add health checks
-      input = { enabled = true },
+      gitbrowse = { enabled = true }, -- Open current file in git remote repo
       notifier = {
         enabled = true,
         timeout = 3000,
@@ -26,173 +24,45 @@ return {
         top_down = false,
       },
       quickfile = { enabled = true },
-      scope = { enabled = true },
-      scroll = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true },
-      styles = {
-        notification = {
-          -- wo = { wrap = true } -- Wrap notifications
-        },
-      },
     },
     keys = {
       -- Other
       {
-        '<leader>z',
+        '<leader>bd',
         function()
-          Snacks.zen()
+          Snacks.dim()
         end,
-        desc = 'Toggle Zen Mode',
-      },
-      {
-        '<leader>Z',
-        function()
-          Snacks.zen.zoom()
-        end,
-        desc = 'Toggle Zoom',
-      },
-      {
-        '<leader>.',
-        function()
-          Snacks.scratch()
-        end,
-        desc = 'Toggle Scratch Buffer',
-      },
-      {
-        '<leader>S',
-        function()
-          Snacks.scratch.select()
-        end,
-        desc = 'Select Scratch Buffer',
+        desc = 'Toggle [D]imming',
       },
       {
         '<leader>n',
         function()
           Snacks.notifier.show_history()
         end,
-        desc = 'Notification History',
+        desc = '[N]otification History',
       },
       {
         '<leader>bd',
         function()
-          Snacks.bufdelete()
+          Snacks.bufdelete.delete()
         end,
-        desc = 'Delete Buffer',
+        desc = '[D]elete [B]uffer',
       },
       {
-        '<leader>cR',
+        '<leader>br',
         function()
           Snacks.rename.rename_file()
         end,
-        desc = 'Rename File',
+        desc = '[R]ename File',
       },
       {
-        '<leader>gB',
+        '<leader>bg',
         function()
           Snacks.gitbrowse()
         end,
-        desc = 'Git Browse',
+        desc = '[G]it Browse',
         mode = { 'n', 'v' },
       },
-      {
-        '<leader>gg',
-        function()
-          Snacks.lazygit()
-        end,
-        desc = 'Lazygit',
-      },
-      {
-        '<leader>un',
-        function()
-          Snacks.notifier.hide()
-        end,
-        desc = 'Dismiss All Notifications',
-      },
-      {
-        '<c-/>',
-        function()
-          Snacks.terminal()
-        end,
-        desc = 'Toggle Terminal',
-      },
-      {
-        '<c-_>',
-        function()
-          Snacks.terminal()
-        end,
-        desc = 'which_key_ignore',
-      },
-      {
-        ']]',
-        function()
-          Snacks.words.jump(vim.v.count1)
-        end,
-        desc = 'Next Reference',
-        mode = { 'n', 't' },
-      },
-      {
-        '[[',
-        function()
-          Snacks.words.jump(-vim.v.count1)
-        end,
-        desc = 'Prev Reference',
-        mode = { 'n', 't' },
-      },
-      {
-        '<leader>N',
-        desc = 'Neovim News',
-        function()
-          Snacks.win {
-            file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-            width = 0.6,
-            height = 0.6,
-            wo = {
-              spell = false,
-              wrap = false,
-              signcolumn = 'yes',
-              statuscolumn = ' ',
-              conceallevel = 3,
-            },
-          }
-        end,
-      },
     },
-    init = function()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'VeryLazy',
-        callback = function()
-          -- Setup some globals for debugging (lazy-loaded)
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
-
-          -- Override print to use snacks for `:=` command
-          if vim.fn.has 'nvim-0.11' == 1 then
-            vim._print = function(_, ...)
-              dd(...)
-            end
-          else
-            vim.print = _G.dd
-          end
-
-          -- Create some toggle mappings
-          Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
-          Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
-          Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
-          Snacks.toggle.diagnostics():map '<leader>ud'
-          Snacks.toggle.line_number():map '<leader>ul'
-          Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
-          Snacks.toggle.treesitter():map '<leader>uT'
-          Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
-          Snacks.toggle.inlay_hints():map '<leader>uh'
-          Snacks.toggle.indent():map '<leader>ug'
-          Snacks.toggle.dim():map '<leader>uD'
-        end,
-      })
-    end,
   },
 }
